dad65647e69ab5ba7d218ea0577a563ae0922fa6
Sanhu Li <lisanhu2014@hotmail.com>
2023-12-09 00:27:55 -0500

    Trying to add a patch for Omni Bots to do cleaning before working on tables. Not successful.

--- Source/Main.cs
+++ Source/Main.cs
@@ -15,33 +15,63 @@ using RimWorld;
 using RimWorld.Planet;
 
 // using System.Reflection;
-// using HarmonyLib;
+using HarmonyLib;
+using CommonSense;
 
-namespace Template
+namespace RPP_Patch
 {
-    [DefOf]
-    public class TemplateDefOf
-    {
-        public static LetterDef success_letter;
-    }
-
-    public class MyMapComponent : MapComponent
-    {
-        public MyMapComponent(Map map) : base(map){}
-        public override void FinalizeInit()
-        {
-            Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
-            Find.LetterStack.ReceiveLetter("Success", TemplateDefOf.success_letter.description, TemplateDefOf.success_letter, null);
-        }
-    }
-
     [StaticConstructorOnStartup]
     public static class Start
     {
         static Start()
         {
-            Log.Message("Mod template loaded successfully!");
+            // Log.Message("Mod template loaded successfully!");
+            Harmony harmony = new Harmony("com.RunningBugs.RppPatch.CommonSense");
+            harmony.PatchAll();
         }
     }
 
+
+    [HarmonyPatch(typeof(Utility), "IncapableOfCleaning")]
+    static class Patch
+    {
+        /**
+         * Original method with minor changes
+         * 
+         * The checks for intelligence is downgrade from Humanlike to ToolUser, remove mental state check
+         */
+        private static bool IncapableOfCleaning(Pawn pawn)
+        {
+            return pawn.def.race == null ||
+                pawn.def.race.intelligence < Intelligence.ToolUser ||
+                pawn.Faction != Faction.OfPlayer ||
+                pawn.RaceProps.intelligence < Intelligence.ToolUser ||
+                pawn.WorkTypeIsDisabled(Utility.CleaningDef) ||
+                pawn.IsBurning() ||
+                pawn.workSettings == null || !pawn.workSettings.WorkIsActive(Utility.CleaningDef);
+        }
+
+        static void Postfix(bool __result, Pawn pawn)
+        {
+            Log.Warning("RPP_Patch: started origin: " + $"{pawn.Name}: {__result}");
+            if (!__result)
+            {
+                return;
+            }
+            if (pawn.def.defName.StartsWith("RPP_Bot_"))
+            {
+                __result = IncapableOfCleaning(pawn);
+                Log.Warning("RPP_Patch: " + pawn.def.defName + $" is ({__result}) incapable of cleaning");
+                Log.Warning("RPP_Patch: " + $"inspecting: {Settings.adv_cleaning}");
+                // Toil FilthList = new Toil();
+                // Job curJob = FilthList.actor.jobs.curJob;
+                // LocalTargetInfo A = curJob.GetTarget(TargetIndex.A);
+                // DoCleanComp comp;
+                // //  {Settings.clean_gizmo} {(comp = A.Thing?.TryGetComp<DoCleanComp>()) == null} {comp.Active}
+                // Log.Warning("RPP_Patch: " + $"inspecting: {Settings.adv_cleaning}");
+                // Log.Warning("RPP_Patch: " + $"inspecting: {Settings.adv_cleaning}");
+            }
+            return;
+        }
+    }
 }

01bf8c16599d73c2265cce449d0990c5a912ac9a
Sanhu Li <lisanhu2014@hotmail.com>
2025-07-31 02:50:17 -0400

    some changes to make it easier to start with

--- 1.6/Source/Main.cs
+++ 1.6/Source/Main.cs
@@ -17,31 +17,29 @@ using RimWorld.Planet;
 // using System.Reflection;
 // using HarmonyLib;
 
-namespace Template
+namespace Template;
+// [DefOf]
+// public class TemplateDefOf
+// {
+//     public static LetterDef success_letter;
+// }
+
+// public class MyMapComponent : MapComponent
+// {
+//     public MyMapComponent(Map map) : base(map){}
+//     public override void FinalizeInit()
+//     {
+//         Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
+//         Find.LetterStack.ReceiveLetter("Success", TemplateDefOf.success_letter.description, TemplateDefOf.success_letter, null);
+//     }
+// }
+
+[StaticConstructorOnStartup]
+public static class Start
 {
-    // [DefOf]
-    // public class TemplateDefOf
-    // {
-    //     public static LetterDef success_letter;
-    // }
-
-    // public class MyMapComponent : MapComponent
-    // {
-    //     public MyMapComponent(Map map) : base(map){}
-    //     public override void FinalizeInit()
-    //     {
-    //         Messages.Message("Success", null, MessageTypeDefOf.PositiveEvent);
-    //         Find.LetterStack.ReceiveLetter("Success", TemplateDefOf.success_letter.description, TemplateDefOf.success_letter, null);
-    //     }
-    // }
-
-    [StaticConstructorOnStartup]
-    public static class Start
+    static Start()
     {
-        static Start()
-        {
-            Log.Message("Mod template loaded successfully!");
-        }
+        Log.Message("Mod template loaded successfully!");
     }
-
 }
+
